92eb1468af45023d18b0becdbcdbcd8d97eb4491
Amoke Emmanuel <emmanuelamoke35@gmail.com>
2025-01-06 17:13:57 +0100

    seventh commit

--- src/app/main/[question_id]/page.tsx
+++ src/app/main/[question_id]/page.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 export default function QuestionId() {
     return(
         <Suspense fallback={`loading`}>
-            
+            <h1 className={`text-center`}>Ask Your Question</h1>
         </Suspense>
     )
 }
